7e8f447f0f26b63f47112c86f532388395fbf4ad
David Baynes <dlbaynes@gmail.com>
2014-01-22 15:30:38 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ClassProject::Application.config.secret_key_base = '26656bd1d13b68436ad795b4afe23370fdfb432bd2c0f75429fcadbca576830bd6045cb627cfca6c9eceef2dcba5a33afc36e46a9fff8ca614ef0314b522e880'
---
+++ db/welcome_helper_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+describe WelcomeHelper do
+
+  it "must be a real test" do
+    flunk "Need real tests"
+  end
+
+end
